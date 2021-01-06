d5f1b2eca46600c97dae75210694a144fbdf132d
HuseyinCivan <huseyincivan@gmail.com>
2021-01-05 19:39:06 -0500

    Ticketing-project-ORM

---
+++ src/main/java/com/cybertek/converter/ProjectDtoConverter.java
@@ -0,0 +1,26 @@
+package com.cybertek.converter;
+
+import com.cybertek.dto.ProjectDTO;
+import com.cybertek.dto.TaskDTO;
+import com.cybertek.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
+    @Override
+    public ProjectDTO convert(String s) {
+        return null;
+    }
+
+//    @Autowired
+//    ProjectService projectService;
+//
+//    @Override
+//    public ProjectDTO convert(String source) {
+//        return projectService.findById(source)   ;
+//    }
+}
